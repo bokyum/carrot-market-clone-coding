0c5a9aeb12b945a983825c9f3062149b1fd45976
bokyum <95bogyeom@gmail.com>
2024-10-27 16:34:14 +0900

    Server Actions

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,27 +3,34 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const handleSubmit = async (data: FormData) => {
+    "use server"; // 함수가 서버에서만 실행되도록 함
+    console.log(data.get("email"), data.get("password"));
+    console.log("i run in the server");
+  };
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={handleSubmit} className="flex flex-col gap-3">
         <FormInput
           type="email"
+          name="email"
           placeholder="Email"
           required={true}
           errors={[]}
         />
         <FormInput
           type="password"
+          name="password"
           placeholder="Password"
           required={true}
           errors={[]}
         />
 
-        <FormBtn loading={false} text="Create Account" />
+        <FormBtn loading={false} text="Log In" />
       </form>
       <SocialLogin />
     </div>
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 interface FormInputProps {
   type: React.InputHTMLAttributes<HTMLInputElement>["type"];
+  name: string;
   placeholder: string;
   required: boolean;
   errors: string[];
@@ -9,6 +10,7 @@ interface FormInputProps {
 
 export default function FormInput({
   type,
+  name,
   placeholder,
   required,
   errors,
@@ -18,6 +20,7 @@ export default function FormInput({
       <input
         className="border-bone h-10 w-full rounded-md bg-transparent px-2 ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
         type={type}
+        name={name}
         placeholder={placeholder}
         required={required}
       />
