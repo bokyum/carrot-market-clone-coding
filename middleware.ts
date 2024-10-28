69875dc7bc544948b4662c76246b9e800d613662
bokyum <95bogyeom@gmail.com>
2024-10-28 17:19:54 +0900

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
 import { NextRequest } from "next/server";
-import getSession from "./libs/session";
 
 // 모든 request에 대해 middleware를 실행
 
-const protectedRoutes = ["/profile", "/settings"];
-
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
   const pathname = request.nextUrl.pathname;
-  if (protectedRoutes.includes(pathname)) {
-    return Response.redirect(new URL("/", request.url), 302);
-  }
+  console.log(pathname);
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
